6e3f7b6075254eb009c0736e0c4017157ba70cec
tanvirurrahmanchowdhury <51336440+tanvirurrahmanchowdhury@users.noreply.github.com>
2021-10-09 00:11:48 -0400

    Update multy.py
    comments added

--- multy.py
+++ multy.py
@@ -9,13 +9,13 @@ import time, math
 
 import multiprocessing as mp
 
-#print(mp.cpu_count())
+#print(mp.cpu_count()) counting the nuber of cpus available
 
 r_a = []
 r_b = []
 r_c = []
 
-def f_1(numbers):
+def f_1(numbers): # a random function
     for n in numbers:
         r_a.append(math.sqrt(n**3))
         
@@ -30,7 +30,8 @@ def f_3(numbers):
 if __name__ == '__main__':
     n_list = list(range(1000000))
     
-    # p1 = mp.Process(target=f_1,args=(n_list,))
+    # begin multiprocessing
+    # p1 = mp.Process(target=f_1,args=(n_list,)) # target = function_name, argrs = argument list as tuple
     # p2 = mp.Process(target=f_2,args=(n_list,))
     # p3 = mp.Process(target=f_3,args=(n_list,))
     
@@ -40,10 +41,14 @@ if __name__ == '__main__':
     # p2.start()
     # p3.start()
     # end = time.time()
+    # print(end-start)
+    # end multiprocessing
     
+    #begin serial processing
     start = time.time()
     f_1(n_list)
     f_2(n_list)
     f_3(n_list)
     end = time.time()
     print(end-start)
+    # end serial processing
