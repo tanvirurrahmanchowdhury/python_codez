9dca71ad9838aa1a76cd227746e3d5e39eb0034b
tanvirurrahmanchowdhury <51336440+tanvirurrahmanchowdhury@users.noreply.github.com>
2021-10-09 00:09:03 -0400

    Add files via upload
    Parallel Processing

---
+++ multy.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  8 23:53:33 2021
+
+@author: Administrator
+"""
+
+import time, math
+
+import multiprocessing as mp
+
+#print(mp.cpu_count())
+
+r_a = []
+r_b = []
+r_c = []
+
+def f_1(numbers):
+    for n in numbers:
+        r_a.append(math.sqrt(n**3))
+        
+def f_2(numbers):
+    for n in numbers:
+        r_b.append(math.sqrt(n**3))
+
+def f_3(numbers):
+    for n in numbers:
+        r_c.append(math.sqrt(n**3))
+        
+if __name__ == '__main__':
+    n_list = list(range(1000000))
+    
+    # p1 = mp.Process(target=f_1,args=(n_list,))
+    # p2 = mp.Process(target=f_2,args=(n_list,))
+    # p3 = mp.Process(target=f_3,args=(n_list,))
+    
+    
+    # start = time.time()
+    # p1.start()
+    # p2.start()
+    # p3.start()
+    # end = time.time()
+    
+    start = time.time()
+    f_1(n_list)
+    f_2(n_list)
+    f_3(n_list)
+    end = time.time()
+    print(end-start)
